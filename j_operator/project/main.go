31b9d85d889595cc4f3bacc6321288ea36ba4e8e
mafa1993 <378909613@qq.com>
2023-04-08 15:35:35 +0800

    自动生成webhook

--- j_operator/project/main.go
+++ j_operator/project/main.go
@@ -96,6 +96,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "MyKind")
 		os.Exit(1)
 	}
+	if err = (&mygroupv1beta1.MyKind{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "MyKind")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
