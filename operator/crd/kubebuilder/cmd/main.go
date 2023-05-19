1bc0631a76975b6c22fd11845fc62cb96aa7a864
mafa1993 <378909613@qq.com>
2023-05-19 15:40:35 +0800

    生成webhook

--- operator/crd/kubebuilder/cmd/main.go
+++ operator/crd/kubebuilder/cmd/main.go
@@ -31,7 +31,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	crdexamplecomv1 "kubebuilder/api/v1"
+	crdv1 "kubebuilder/api/v1"
 	"kubebuilder/internal/controller"
 	//+kubebuilder:scaffold:imports
 )
@@ -44,7 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	utilruntime.Must(crdexamplecomv1.AddToScheme(scheme))
+	utilruntime.Must(crdv1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -71,7 +71,7 @@ func main() {
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "76eb6a2f.crd.example.com",
+		LeaderElectionID:       "76eb6a2f.example.com",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
@@ -92,8 +92,12 @@ func main() {
 	if err = (&controller.AppReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {  // 调用controller的路基
+	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "App")
+		os.Exit(1)
+	}
+	if err = (&crdv1.App{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "App")
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
--- operator/crd/kubebuilder/internal/controller/app_controller.go
+++ operator/crd/kubebuilder/internal/controller/app_controller.go
@@ -24,7 +24,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	crdexamplecomv1 "kubebuilder/api/v1"
+	crdv1 "kubebuilder/api/v1"
 )
 
 // AppReconciler reconciles a App object
@@ -33,9 +33,9 @@ type AppReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=crd.example.com.crd.example.com,resources=apps,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=crd.example.com.crd.example.com,resources=apps/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=crd.example.com.crd.example.com,resources=apps/finalizers,verbs=update
+//+kubebuilder:rbac:groups=crd.example.com,resources=apps,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=crd.example.com,resources=apps/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=crd.example.com,resources=apps/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -46,22 +46,17 @@ type AppReconciler struct {
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.4/pkg/reconcile
-// controller业务逻辑的实现
 func (r *AppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
 	// TODO(user): your logic here
 
-	return ctrl.Result{}, nil  // result有两个属性，一个是是否重试，一个是间隔多久重试
+	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
-// 将controller注册到manager中
 func (r *AppReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).  // 创建builder
-		For(&crdexamplecomv1.App{}). // 将自定义对象放入builder的ForInput属性，ownsInput管理 ownref 的关联子对象 Watches 用来设置其他关注的对象  withFilter是用来设置prdicate withoptions 用来设置controller的一些配置，例如worker数量等
-		Complete(r)  // 这里面会调用Reconcile，里面对controller进行了初始化，并将controller add到了manager，controller.do 存放了Reconcile方法
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&crdv1.App{}).
+		Complete(r)
 }
-// complete中包含两个主要过程，do controller 和 do watch
-// do watch 中实现了事件的监听和处理  predicate用来过滤事件，controller start来开始事件的监听
-// 事件的入队列，是用Request对象来存储的，包含name和namespace属性
