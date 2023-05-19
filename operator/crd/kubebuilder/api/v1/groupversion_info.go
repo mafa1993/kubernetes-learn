1bdcf840e6c19e796b92c1ff1a0fdf2350b7c5cc
mafa1993 <378909613@qq.com>
2023-05-19 15:39:59 +0800

    修改kind 重新生成

--- operator/crd/kubebuilder/api/v1/groupversion_info.go
+++ operator/crd/kubebuilder/api/v1/groupversion_info.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package v1 contains API Schema definitions for the crd.example.com v1 API group
+// Package v1 contains API Schema definitions for the crd v1 API group
 // +kubebuilder:object:generate=true
 // +groupName=crd.example.com
 package v1
