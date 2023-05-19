0eb2822b6477b15bf7eed32d1c3820f959070136
mafa1993 <378909613@qq.com>
2023-05-19 15:56:25 +0800

    增加webhook

--- operator/crd/kubebuilder/api/v1/app_webhook.go
+++ operator/crd/kubebuilder/api/v1/app_webhook.go
@@ -17,8 +17,10 @@ limitations under the License.
 package v1
 
 import (
+	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
 // log is for logging in this package.
@@ -31,3 +33,43 @@ func (r *App) SetupWebhookWithManager(mgr ctrl.Manager) error {
 }
 
 // TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+
+//+kubebuilder:webhook:path=/mutate-crd-example-com-v1-app,mutating=true,failurePolicy=fail,sideEffects=None,groups=crd.example.com,resources=apps,verbs=create;update,versions=v1,name=mapp.kb.io,admissionReviewVersions=v1
+
+var _ webhook.Defaulter = &App{}
+
+// Default implements webhook.Defaulter so a webhook will be registered for the type
+func (r *App) Default() {
+	applog.Info("default", "name", r.Name)
+
+	// TODO(user): fill in your defaulting logic.
+}
+
+// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
+//+kubebuilder:webhook:path=/validate-crd-example-com-v1-app,mutating=false,failurePolicy=fail,sideEffects=None,groups=crd.example.com,resources=apps,verbs=create;update,versions=v1,name=vapp.kb.io,admissionReviewVersions=v1
+
+var _ webhook.Validator = &App{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (r *App) ValidateCreate() error {
+	applog.Info("validate create", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object creation.
+	return nil
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (r *App) ValidateUpdate(old runtime.Object) error {
+	applog.Info("validate update", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object update.
+	return nil
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
+func (r *App) ValidateDelete() error {
+	applog.Info("validate delete", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object deletion.
+	return nil
+}
